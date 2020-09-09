79cce9b12cc663cbb80d598f978c91dff9b2aa57
Tomoyuki Hata <hato6502@gmail.com>
2020-09-09 23:59:29 +0900

    feat: add unwrap button

--- src/index.ts
+++ src/index.ts
@@ -62,25 +62,75 @@ class EditorJSStyle implements InlineTool {
   checkState() {
     const span = this.api.selection.findParentTag('SPAN', 'editorjs-style');
 
-    this.button.classList.toggle(
-      this.api.styles.inlineToolButtonActive,
-      Boolean(span)
-    );
     this.actions.innerHTML = '';
 
     if (span) {
-      this.actions.append(document.createTextNode('Style'));
+      this.actions.innerHTML = `
+        <div style="display: flex; align-items: center; justify-content: space-between; ">
+          <div>Style settings</div>
 
-      const input = document.createElement('input');
+          <button class="delete-button ${this.api.styles.settingsButton}" type="button">
+            <svg class="icon" height="24" viewBox="0 0 24 24" width="24">
+              <path d="M0 0h24v24H0z" fill="none"/>
+              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+            </svg>
+          </button>
+        </div>
+        <input class="style-input ${this.api.styles.input}">
+      `;
 
-      input.classList.add(this.api.styles.input);
-      input.value = span.getAttribute('style') ?? '';
+      const deleteButton = this.actions.querySelector(
+        '.delete-button'
+      ) as HTMLButtonElement | null;
+      const styleInput = this.actions.querySelector(
+        '.style-input'
+      ) as HTMLInputElement | null;
 
-      input.addEventListener('input', () =>
-        span.setAttribute('style', input.value)
+      if (!deleteButton || !styleInput) {
+        throw new Error("Couldn't render actions for editorjs-style. ");
+      }
+
+      deleteButton.addEventListener('click', () => {
+        const clonedNodes = Array.from(span.childNodes).map((node) =>
+          node.cloneNode(true)
+        );
+
+        clonedNodes.forEach((node) =>
+          span.parentNode?.insertBefore(node, span)
+        );
+        span.remove();
+
+        if (clonedNodes.length === 0) {
+          return;
+        }
+
+        const selection = window.getSelection();
+
+        if (!selection) {
+          throw new Error(
+            "Couldn't select unwrapped editorjs-style contents. "
+          );
+        }
+
+        selection.removeAllRanges();
+
+        const range = new Range();
+
+        range.setStartBefore(clonedNodes[0]);
+        range.setEndAfter(clonedNodes[clonedNodes.length - 1]);
+
+        selection.addRange(range);
+      });
+
+      this.api.tooltip.onHover(deleteButton, 'Delete style', {
+        placement: 'top',
+      });
+
+      styleInput.value = span.getAttribute('style') ?? '';
+
+      styleInput.addEventListener('input', () =>
+        span.setAttribute('style', styleInput.value)
       );
-
-      this.actions.append(input);
     }
 
     return Boolean(span);
@@ -142,8 +192,9 @@ class EditorJSStyle implements InlineTool {
 
     button.classList.add(this.api.styles.inlineToolButton);
     button.type = 'button';
+
     button.innerHTML = `
-      <svg class="icon" viewBox="0 0 24 24">
+      <svg class="icon" height="24" viewBox="0 0 24 24" width="24">
         <path d="M0 0h24v24H0z" fill="none"/>
         <path d="M2.53 19.65l1.34.56v-9.03l-2.43 5.86c-.41 1.02.08 2.19 1.09 2.61zm19.5-3.7L17.07 3.98c-.31-.75-1.04-1.21-1.81-1.23-.26 0-.53.04-.79.15L7.1 5.95c-.75.31-1.21 1.03-1.23 1.8-.01.27.04.54.15.8l4.96 11.97c.31.76 1.05 1.22 1.83 1.23.26 0 .52-.05.77-.15l7.36-3.05c1.02-.42 1.51-1.59 1.09-2.6zM7.88 8.75c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-2 11c0 1.1.9 2 2 2h1.45l-3.45-8.34v6.34z"/>
       </svg>
