81a44760a96710d0c1a4a61e985dc527d2ad1b46
Tomoyuki Hata <hato6502@gmail.com>
2020-09-19 01:32:35 +0900

    delete zero width space

--- src/EditorJSStyleElement.ts
+++ src/EditorJSStyleElement.ts
@@ -4,28 +4,6 @@ class EditorJSStyleElement extends HTMLElement {
 
     // To prevent Editor.js keydown event
     this.addEventListener('keydown', (event) => event.stopPropagation());
-
-    const mutationObserver = new MutationObserver(() => {
-      if (
-        this.firstChild?.nodeName !== '#text' ||
-        this.firstChild?.textContent?.slice(0, 1) !== '\u200b'
-      ) {
-        this.prepend('\u200b');
-      }
-
-      if (
-        this.lastChild?.nodeName !== '#text' ||
-        this.lastChild?.textContent?.slice(-1) !== '\u200b'
-      ) {
-        this.append('\u200b');
-      }
-    });
-
-    mutationObserver.observe(this, {
-      characterData: true,
-      childList: true,
-      subtree: true,
-    });
   }
 }
 
